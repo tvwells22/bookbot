128d573aab684f2681d1503a51e905316f48a65d
tvwells22 <tvwells22@gmail.com>
2024-03-27 13:14:38 -0600

    Defined but havent implemented char_count function

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    char_count = get_char_count(text.lower())
+
+def get_char_count(text):
+    ...
 
 
 def get_num_words(text):
-    words = text.split()
-    return len(words)
+    return len(text.split())
 
 
 def get_book_text(path):
