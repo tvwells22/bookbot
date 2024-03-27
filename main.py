ebcaab93d9b675d0feb1949e2b096bdd61654c30
tvwells22 <tvwells22@gmail.com>
2024-03-27 13:39:15 -0600

    Formatted printed report and defined but didnt create sorting function

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
-    char_count = get_char_count(text.lower())
+    char_count = get_char_count(text)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
 
-def get_char_count(text):
+
+def sorted_characters(text):
     ...
 
+def get_char_count(text):
+    chars = {}
+    for letter in text.lower():
+        if letter in chars:
+            chars[letter] += 1
+        else:
+            chars[letter] = 1
+    return chars
 
 def get_num_words(text):
     return len(text.split())
